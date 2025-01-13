f1b775493947fc7f149f9d73c75853f3d92f2598
Vedant Hippalgave <158559140+vedanth2174@users.noreply.github.com>
2025-01-13 19:40:58 +0000

    started

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 154153513
+let accountEmail = "vedanthstudies@gmail.com"
+var accountPassword = "Vedant#1103"
+accountCity = "City"
+
+//const accountId = 6541651
+
+console.log(accountId);
