377a35357bf10c2ec3ba6e0d7b9645024f8d21a9
Vedant Hippalgave <158559140+vedanth2174@users.noreply.github.com>
2025-01-14 16:59:56 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict" //treal all JS as newer version
+// alert("hii i am vedant")
+// keep code readibility high
+
+let name = "vedant"
+let age = 23
+let isLoggednIn = true
+
+//prefer writing string in double quotes
+//null is standalone value
+//undefined is given if no value is defined
+//symbol is also a datatype used mostly in react ( unique)
+
+//objects are most important
+//function typeof is used to display datatype of any value
+console.log(typeof null) //datatype object
+
+
+
+
+
