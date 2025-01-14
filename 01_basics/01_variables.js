1af27fb598d5551adc5443610edf58d340731ba1
Vedant Hippalgave <158559140+vedanth2174@users.noreply.github.com>
2025-01-14 16:48:12 +0000

    variables complete

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,7 +2,15 @@ const accountId = 154153513
 let accountEmail = "vedanthstudies@gmail.com"
 var accountPassword = "Vedant#1103"
 accountCity = "City"
+let accountState
 
 //const accountId = 6541651
 
+/*Preferred not to use var due to scope resolution problem */
+
+accountEmail = "abc@gmail.com"
+accountPassword = "Ved#1109"
+accountCity = "Banglore"
+
 console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
