6ca949be894afe250db2016020c430b2122c7952
Vedant Hippalgave <158559140+vedanth2174@users.noreply.github.com>
2025-01-14 18:54:00 +0000

    string complete

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,22 @@
+const name = "vedant"
+const number = 50
+console.log(name+number+"  54")
+
+console.log(`Hii, my name is ${name}`) //`` are back ticks
+
+const newName = new String("Mayur")
+
+console.log(newName.length)
+console.log(newName.toUpperCase())
+console.log(newName.charAt(3))
+console.log(newName.indexOf('y'))
+
+let ne = newName.substring(0,3)
+console.log(ne)
+
+let neww = newName.slice(-5,3)
+console.log(neww)
+
+let enw = "          sgs r       "
+console.log(enw.trim())\
+// remove all unwanted spaces
