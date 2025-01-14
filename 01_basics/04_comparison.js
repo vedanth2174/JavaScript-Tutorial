c1f182090efe9ee11dafb85b36c0c17f4f4d3113
Vedant Hippalgave <158559140+vedanth2174@users.noreply.github.com>
2025-01-14 17:28:05 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,13 @@
+console.log("2">1)
+console.log("02">1)
+
+//make sure datatypes are same of the data that you are comparing
+
+console.log(null>0)
+console.log(null<0)
+console.log(null==0)
+console.log(null>=0)
+
+console.log(undefined==0)
+console.log("2"==2)
+console.log("2"===2) //also checks datatypes
