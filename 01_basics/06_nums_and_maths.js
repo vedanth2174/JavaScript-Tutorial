98e16935896d26be16122c00ba3f5e5dbe20a686
Vedant Hippalgave <158559140+vedanth2174@users.noreply.github.com>
2025-01-15 16:11:02 +0000

    Maths and Number

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,24 @@
+const score = 400
+const balance = new Number(200)
+
+console.log(score)
+console.log(balance)
+
+console.log(balance.toString())
+console.log(balance.toString().length)
+const number = 54.326
+console.log(number.toPrecision(3))
+
+const hundreds = 1000000000
+console.log(hundreds.toLocaleString('en-IN'))
+
+//+++++++++++++++++++++++++++++++++Maths+++++++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math.round(4.56))
+console.log(Math.ceil(4.56))
+console.log(Math.floor(4.56))
+console.log(Math.random()*100)
+
+//Math.random() gives values between 0 and 1
+// console.log(Math.floor(Math.random()*(max-min+1))+min)
+// This is to get random number between max and min value
